ebe4541f3fc31ae7a60ac92488568f37aea55161
mgthomas99 <m.g.thomas99@gmail.com>
2018-11-02 00:08:30 +0000

    testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello, world!");
+import * as fs from "fs";
+import * as nearley from "nearley";
+
+const grammar = nearley.Grammar.fromCompiled(require("./grammar/grammar"));
+const parser = new nearley.Parser(grammar);
+
+export function evaluateFile(path: string)
+: (string) {
+  const content = fs.readFileSync(path, { encoding: "utf-8" });
+  return evaluate(content);
+}
+
+export function evaluate(code: string)
+: (string) {
+  parser.feed(code);
+  const results = parser.finish();
+  const result = results[0];
+  return JSON.stringify(result, null, 4);
+}
+
+console.log(evaluate(`"Hello, world!"`));
