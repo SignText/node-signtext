7f77d966733f095ef0fc3d9979394fff44e16506
mgthomas99 <m.g.thomas99@gmail.com>
2018-11-11 23:06:50 +0000

    fixed evaluator

--- src/context/runtime.ts
+++ src/context/runtime.ts
@@ -1,9 +1,11 @@
 import { Mutex } from "async-mutex";
 import * as nearley from "nearley";
 
+import { FunctionCall } from "./lexer/function-call";
 import { Namespace } from "./object/namespace";
+import { Primitive } from "./object/primitive";
 
-const grammar = nearley.Grammar.fromCompiled(require("./grammar/grammar"));
+const grammar = nearley.Grammar.fromCompiled(require("./../grammar/grammar"));
 
 export class Runtime {
 
@@ -15,18 +17,30 @@ export class Runtime {
     this.mutex = new Mutex();
   }
 
-  public eval(x: string, ns?: Namespace)
-  : (Promise<string>) {
+  public async eval(x: FunctionCall | string, ns?: Namespace)
+  : (Promise<Primitive>) {
     if (typeof ns === "undefined") return this.eval(x, {});
+    if (typeof x !== "string") return this._eval(x, ns);
 
-    return this.mutex.acquire().then((done) => {
-      this.parser.feed(x);
-      this.parser.finish();
+    const release = await this.mutex.acquire();
+    this.parser.feed(x);
+    this.parser.finish();
 
-      const result = this.parser.results[0];
-      done();
-      return result;
-    });
+    const result = this.parser.results[0][0];
+    release();
+    return this._eval(result, ns);
+  }
+
+  private _eval(x: FunctionCall | Primitive, ns: Namespace)
+  : (Primitive | string) {
+    if (typeof x !== "object") return x;
+
+    const params = x.parameters.map(y => this._eval(y, ns));
+    const fn = ns[x.identifier];
+    if (typeof fn !== "function") {
+      throw new Error(`Cannot call type '${typeof fn}'`);
+    }
+    return fn(...params);
   }
 
 }
