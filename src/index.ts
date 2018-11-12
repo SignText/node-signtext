1ffa695083d5c5d358bf41c1b6a6de92ade002b2
mgthomas99 <m.g.thomas99@gmail.com>
2018-11-12 18:43:12 +0000

    hide unnecessary imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-export * from "./lang/token/function-call-token";
-export * from "./lang/token/token-type";
-export * from "./lang/token/variable-call-token";
-export * from "./lang/context";
 export * from "./lang/namespace";
 export * from "./lang/primitive";
+export * from "./runtime";
 
 import * as std from "./stdlib";
 export const stdlib = std;
