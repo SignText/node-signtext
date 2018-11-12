76364e458b8f5a50eda1516e85913002ffb93b67
mgthomas99 <m.g.thomas99@gmail.com>
2018-11-12 18:42:04 +0000

    refactored

--- src/lang/context.ts
+++ src/lang/context.ts
@@ -7,20 +7,40 @@ export class Context {
   public async eval(x: Primitive | TokenType, ns: Namespace)
   : (Promise<NamespaceKey | string>) {
     if (typeof x === "object") {
-      if (x.type === "FunctionCall") {
-        const item = ns[x.ctx.identifier];
-        const args = await Promise.all(x.ctx.parameters
-            .map((arg) => this.eval(arg, ns)));
-        if (typeof item === "function") {
-          return item(...args);
-        } else {
-          throw new Error(`Expected function, found ${typeof item}`);
-        }
-      } else if (x.type === "VariableCall") {
-        return ns[x.ctx.identifier];
+      if (x.type === "Call") {
+        return await this.call(ns, x.ctx.identifier, ...x.ctx.parameters);
+      } else if (x.type === "Get") {
+        return this.get(ns, x.ctx.identifier);
       }
     } else {
       return x;
+    }
+  }
+
+  public async call(ns: Namespace, item: string, ...params: any[])
+  : (Promise<Primitive>) {
+    const ex = this.get(ns, item);
+    if (typeof ex === "function") {
+      return ex(...params);
+    } else {
+      throw new Error("Expected function, received " + (typeof ex));
+    }
+  }
+
+  public get(ns: Namespace, item: string)
+  : (NamespaceKey) {
+    const parts = item.split(/\./g);
+
+    if (parts.length === 1) {
+      return ns[parts[0]];
+    } else {
+      let res: any = ns;
+
+      while (parts.length > 0) {
+        const part = parts.splice(0, 1)[0];
+        res = this.get(res, part);
+      }
+      return res;
     }
   }
 
