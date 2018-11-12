c664bc44c5a0eb6177cb0d75f3a14745e27473d2
mgthomas99 <m.g.thomas99@gmail.com>
2018-11-12 02:06:54 +0000

    use context

--- src/runtime.ts
+++ src/runtime.ts
@@ -1,9 +1,8 @@
 import { Mutex } from "async-mutex";
 import * as nearley from "nearley";
 
-import { FunctionCall } from "./lang/token/function-call";
 import { Namespace } from "./lang/namespace";
-import { Primitive } from "./lang/primitive";
+import { Context } from "./lang/context";
 
 const grammar = nearley.Grammar.fromCompiled(require("./lang/grammar/grammar"));
 
@@ -11,9 +10,11 @@ export class Runtime {
 
   private readonly parser: nearley.Parser;
   private readonly mutex: Mutex;
+  private readonly ctx: Context;
 
   public constructor() {
     this.parser = new nearley.Parser(grammar);
+    this.ctx = new Context();
     this.mutex = new Mutex();
   }
 
@@ -28,10 +29,9 @@ export class Runtime {
    * @return    {Promise<Primitive>}
    *            A `Promise` which eventually resolves to the final evaluation.
    */
-  public async eval(x: FunctionCall | string, ns?: Namespace)
-  : (Promise<Primitive>) {
+  public async eval(x: string, ns?: Namespace)
+  : (Promise<string>) {
     if (typeof ns === "undefined") return this.eval(x, {});
-    if (typeof x !== "string") return this._eval(x, ns);
 
     const release = await this.mutex.acquire();
     this.parser.feed(x);
@@ -39,19 +39,8 @@ export class Runtime {
 
     const result = this.parser.results[0][0];
     release();
-    return this._eval(result, ns);
-  }
-
-  private _eval(x: FunctionCall | Primitive, ns: Namespace)
-  : (Primitive | string) {
-    if (typeof x !== "object") return x;
-
-    const params = x.parameters.map(y => this._eval(y, ns));
-    const fn = ns[x.identifier];
-    if (typeof fn !== "function") {
-      throw new Error(`Cannot call type '${typeof fn}'`);
-    }
-    return fn(...params);
+    const res = await this.ctx.eval(result, ns);
+    return String(res);
   }
 
 }
