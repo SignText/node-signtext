3f688f929f69a07b0a6afa1ea2bb4d5cabefd316
gthomas <gthomas@geologging.com>
2019-02-04 16:57:49 +0000

    removed std lib

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
+export * from "./lang/context";
 export * from "./lang/namespace";
 export * from "./lang/primitive";
 export * from "./runtime";
-
-import * as std from "./stdlib";
-export const stdlib = std;
--- src/stdlib/index.ts
+++
@@ -1,8 +0,0 @@
-import chalk from "chalk";
-
-export const Chalk = chalk;
-
-export function concat(...args: any[])
-: (string) {
-  return args.join("");
-}
